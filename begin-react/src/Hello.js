a1e0f585359bf2d23b684bccd15ccf505a50eb71
Moon Beob Sik <tjswl756@naver.com>
2021-04-29 17:02:13 +0900

    Conditional rendering

--- begin-react/src/Hello.js
+++ begin-react/src/Hello.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Hello({ color, name }) {
-    return <div style={{ color }}>안녕하세요 {name}</div>
+function Hello({ color, name, isSpecial }) {
+    return (
+        <div style={{ color }}>
+            {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+        </div>
+    );
 }
 
 Hello.defaultProps = {
     name: 'noName'
 }
+
 export default Hello;
