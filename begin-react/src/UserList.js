e54d41e52119bad959d6086340f6c0e2cd2a6591
Moon Beob Sik <tjswl756@naver.com>
2021-04-29 20:25:37 +0900

    make variable in component by useref

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -7,28 +7,11 @@ function User({ user }) {
         </div>
     );
 }
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'velopert',
-            email: 'public.velopert@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+function UserList({ users }) {
     return (
         <div>
-            {users.map((user, index) => (
-                <User user={user} key={index} />
+            {users.map(user => (
+                <User user={user} key={user.id} />
             ))}
         </div>
     );
